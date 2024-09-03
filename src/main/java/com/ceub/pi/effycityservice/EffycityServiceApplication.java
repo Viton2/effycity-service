6364ecb284efbe5d72d6e6045c729b6fe2a4ec6d
Vitor Oliveira <vitor.oliveira@digitalscutum.com.br>
2024-09-03 20:24:41 -0300

    commit

---
+++ src/main/java/com/ceub/pi/effycityservice/EffycityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ceub.pi.effycityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffycityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffycityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceub/pi/effycityservice/EffycityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceub.pi.effycityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffycityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
