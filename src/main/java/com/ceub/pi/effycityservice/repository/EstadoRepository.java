95cc828ab260fccf899fbaae089c0d478ad2a305
Vitor Oliveira <vitor.oliveira@digitalscutum.com.br>
2024-12-12 15:27:56 -0300

    Sorted State and County returns

--- src/main/java/com/ceub/pi/effycityservice/repository/EstadoRepository.java
+++ src/main/java/com/ceub/pi/effycityservice/repository/EstadoRepository.java
@@ -2,10 +2,16 @@ package com.ceub.pi.effycityservice.repository;
 
 import com.ceub.pi.effycityservice.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+    @Query("SELECT e FROM Estado e ORDER BY e.noEstado ASC")
+    List<Estado> findAllOrderByNoEstadoAsc();
 
 }
